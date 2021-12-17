557b3c665bc3787ca68256879b01d83314f8ca70
alvedge <alvedge@gmail.com>
2021-12-17 02:55:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CrazyBallGame
+
+Developed with Unreal Engine 4
---
+++ Source/CrazyBallGame/CrazyBallGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrazyBallGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAZYBALLGAME_API ACrazyBallGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
