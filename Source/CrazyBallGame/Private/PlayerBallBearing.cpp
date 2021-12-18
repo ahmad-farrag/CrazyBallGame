48af7f5b8f138f2902035b8efffb564879e6f0d9
alvedge <alvedge@gmail.com>
2021-12-18 14:45:31 +0200

    Setting up Default Pawn Class and Player Input
    Created a Blueprint class based on the CrazyBallGameGameMode and changed the default Pawn Class to be BP_PlayerBallBearing. and then went to World Settings and GameMode Override-> BP_CrazyBallGameMode .. now our pawn is in the world.

--- Source/CrazyBallGame/Private/PlayerBallBearing.cpp
+++ Source/CrazyBallGame/Private/PlayerBallBearing.cpp
@@ -2,4 +2,68 @@
 
 
 #include "PlayerBallBearing.h"
+#include "GameFramework/PlayerInput.h"
+#include "Components/InputComponent.h"
 
+APlayerBallBearing::APlayerBallBearing()
+{
+	Magnetized = false;
+}
+//Establish the default pawn input bindings for a PlayerBallBearing.
+static void InitializeDefaultPawnInputBindings()
+{
+	static bool bindingsAdded = false;
+
+	if (bindingsAdded == false)
+	{
+		bindingsAdded = true;
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLongitudinally", EKeys::W, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLongitudinally", EKeys::S, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLongitudinally", EKeys::Up, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLongitudinally", EKeys::Down, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLongitudinally", EKeys::Gamepad_LeftY, 1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLaterally", EKeys::A, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLaterally", EKeys::D, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLaterally", EKeys::Left, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLaterally", EKeys::Right, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("BallBearing_MoveLaterally", EKeys::Gamepad_LeftX, 1.f));
+
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("BallBearing_Jump", EKeys::Enter));
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("BallBearing_Dash", EKeys::SpaceBar));
+	}
+}
+
+//Called to bind functionality to input.
+void APlayerBallBearing::SetupPlayerInputComponent(UInputComponent* playerInputComponent)
+{
+	check(playerInputComponent != nullptr);
+
+	Super::SetupPlayerInputComponent(playerInputComponent);
+
+	InitializeDefaultPawnInputBindings();
+
+	playerInputComponent->BindAxis("BallBearing_MoveLongitudinally", this, &APlayerBallBearing::MoveLongitudinally);
+	playerInputComponent->BindAxis("BallBearing_MoveLaterally", this, &APlayerBallBearing::MoveLaterally);
+
+	playerInputComponent->BindAction("BallBearing_Jump", EInputEvent::IE_Pressed, this, &APlayerBallBearing::Jump);
+	playerInputComponent->BindAction("BallBearing_Dash", EInputEvent::IE_Pressed, this, &APlayerBallBearing::Dash);
+}
+
+//Have the ball bearing perform a jump.
+void APlayerBallBearing::Jump()
+{
+}
+
+//Have the ball bearing perform a dash.
+void APlayerBallBearing::Dash()
+{
+}
+
+void APlayerBallBearing::Tick(float deltaSeconds)
+{
+	Super::Tick(deltaSeconds);
+
+	BallMesh->AddForce(FVector(InputLongitude, InputLatitude, 0.0f) * ControllerForce * BallMesh->GetMass());
+}
--- Source/CrazyBallGame/Private/PlayerBallBearing.h
+++ Source/CrazyBallGame/Private/PlayerBallBearing.h
@@ -13,5 +13,61 @@ UCLASS()
 class APlayerBallBearing : public ABallBearing
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	// Sets default values for this pawn's properties.
+	APlayerBallBearing();
+
+	// How much force to use to push the ball bearing around.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = BallBearing)
+		float ControllerForce = 250.0f;
+
+	// How much force to use to push the ball bearing into the air.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = BallBearing)
+		float JumpForce = 50.0f;
+
+	// How much force to use to have the ball bearing dash.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = BallBearing)
+		float DashForce = 150.0f;
+
+	// The maximum speed in meters per second.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = BallBearing)
+		float MaximumSpeed = 4.0f;
+protected:
+
+	// Control the movement of the ball bearing, called every frame.
+	virtual void Tick(float deltaSeconds) override;
+
+	// Called to bind functionality to input.
+	virtual void SetupPlayerInputComponent(class UInputComponent* playerInputComponent) override;
+
+private:
+
+	// Move the ball bearing with the given force longitudinally on the X axis.
+	void MoveLongitudinally(float value)
+	{
+		InputLongitude = value;
+	}
+
+	// Move the ball bearing with the given force longitudinally on the Y axis.
+	void MoveLaterally(float value)
+	{
+		InputLatitude = value;
+	}
+
+	// Have the ball bearing perform a jump.
+	void Jump();
+
+	// Have the ball bearing perform a dash.
+	void Dash();
+
+	// The current longitude input received from the player.
+	float InputLongitude = 0.0f;
+
+	// The current latitude input received from the player.
+	float InputLatitude = 0.0f;
+
+	// Timer used to control the dashing of the ball bearing.
+	float DashTimer = 0.0f;
 };
